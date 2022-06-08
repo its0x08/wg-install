c0fea117632aa0a5dbfef8772c39e06fe45f1d24
0x08 <its0x08@users.noreply.github.com>
2022-06-08 20:10:25 +0300

    chore: Remove unused variable

--- wg-install.sh
+++ wg-install.sh
@@ -4,9 +4,9 @@
 WG_CONFIG="/etc/wireguard/wg0.conf"
 
 function generate_port {
-	local random_int port
+	local random_int
 	random_int="$(shuf -i 2000-65535 -n 1)"
-	port="$(ss -lau | grep "$random_int" > /dev/null)"
+	ss -lau | grep "$random_int" > /dev/null
 	if [[ "$?" == 1 ]]; then
 		echo "$random_int"
 	else
